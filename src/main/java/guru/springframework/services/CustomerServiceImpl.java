f8929487c14a2df761125d1b170bf45a948f7f59
Panya Boonyakulsrirung <panya.javamania@gmail.com>
2018-09-17 17:29:06 +0700

    first commit

---
+++ src/main/java/guru/springframework/services/CustomerServiceImpl.java
@@ -0,0 +1,32 @@
+package guru.springframework.services;
+
+import guru.springframework.domain.Customer;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+    @Override
+    public Customer saveOrUpdateCustomer() {
+        System.out.println("CustomerServiceImpl - save or update customer");
+        return null;
+    }
+
+    @Override
+    public void deleteCustomer(Long id) {
+        System.out.println("CustomerServiceImpl - deleteCustomer");
+    }
+
+    @Override
+    public List<Customer> listAll() {
+        System.out.println("CustomerServiceImpl - listAll");
+        return null;
+    }
+
+    @Override
+    public Customer getById(Long id) {
+        System.out.println("CustomerServiceImpl - getById");
+        return null;
+    }
+}
